b469a057ec4b19b16fecf18707b33d2cc1690663
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2007-09-12 09:22:35 +0000

    More cpp files
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@211 3f9d24b3-d911-0410-9091-ffa98efebf22

---
+++ network_communication.cpp
@@ -0,0 +1,33 @@
+#ifdef NETWORK_SUPPORT
+
+#include "network_communication.h"
+#include "communicator.h"
+#include <QCoreApplication>
+#include <QTcpSocket>
+static QCoreApplication * qapp;
+
+void init_qt(int argc, char ** argv){
+  qapp = new QCoreApplication(argc,argv);
+}
+
+void * attempt_connection(char * server, int server_port){
+  QTcpSocket * socket = new QTcpSocket;
+    socket->connectToHost(server,server_port);
+  if(socket->waitForConnected()){
+    printf("Valid socket!\n");
+    return socket;
+  }
+  delete socket;
+  return NULL;
+}
+
+void wait_for_server_instructions(void * _socket){
+  QTcpSocket * socket = reinterpret_cast<QTcpSocket *>(_socket);
+  
+}
+
+void cleanup_qt(){
+  delete qapp;
+}
+
+#endif
