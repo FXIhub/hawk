62f333d7f0f91d59a69fdbc18f4323cd317f66ca
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2008-04-10 20:20:13 +0000

    First commit
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@264 3f9d24b3-d911-0410-9091-ffa98efebf22

---
+++ utils/image_fresnel.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "spimage.h"
+
+int main(int argc, char ** argv){
+  Image * a, * out;
+  int i;
+  if(argc < 3){
+    printf("Usage: image_fft <input image> <output image>\n");
+    return 0;
+  }
+  a = sp_image_read(argv[1],0);
+  if(a->shifted){
+    a = sp_image_shift(a);
+  }
+  out = sp_image_fft(a);
+  sp_image_write(out,argv[2],0);
+  return 0;
+}
