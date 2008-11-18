aa56df4e26d73179fd34994f25eedf06ad101b69
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2008-11-18 15:42:58 +0000

    Read GMD data on the VMI converter
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@364 3f9d24b3-d911-0410-9091-ffa98efebf22

--- utils/VMIraw2tiff.c
+++ utils/VMIraw2tiff.c
@@ -8,16 +8,64 @@ typedef struct{
   int numimages,exptime,gain,threshold,countingmode,bgsub,counts,framerate,missed,timestamp,streamexists ;
 }ExtraData;
 
+typedef struct{
+  int col1,bunchid,col3,col4,hour,minute,second;
+  float col6,ion_tunnel,e_tunnel,ion_hall,e_hall;
+}GMDData;
+
+
+GMDData * get_values_from_GMD(FILE * GMD,int bunchid){
+  if(!GMD){
+    return 0;
+  }
+  printf("Searching for intensity on GMD...\n");
+  fseek(GMD,0,SEEK_SET);
+  char line[1024];
+  char sid[1024];
+  sprintf(sid," %d",bunchid);
+  while(fgets(line,1024,GMD)){
+    if(strstr(line,sid)){
+      GMDData * res = malloc(sizeof(GMDData));
+
+      /* bunch id found on this line.
+	 Lets parse it */
+      printf("Found line: %s\n",line);
+      sscanf(line,"%d %d %d %d %d:%d:%d %f %f %f %f %f",&res->col1,&res->bunchid,&res->col3,&res->col4,&res->hour,&res->minute,&res->second,&res->col6,&res->ion_tunnel,&res->e_tunnel,&res->ion_hall,&res->e_hall);
+      return res;         
+    }
+  }
+  return 0;
+}
+
+/* This function transforms filename.ext into filename-x.ext */
+void append_tag(char * output,char * basename, char * x){
+  char * p = strstr(basename,".");
+  if(!p){
+    sprintf(output,"%s-%s",basename,x);
+    return;
+  }
+  *p = 0;
+  sprintf(output,"%s-%s.%s",basename,x,p+1);
+  *p = '.';  
+}
+
 int main(int argc, char ** argv){
   Image * image;
-  if(argc != 3 ){
-    printf("VMIraw2tiff <file.raw> <file.tiff>\n");
+  if(argc != 3 && argc != 4){
+    printf("VMIraw2tiff <file.raw> <file.tiff> [GMD.txt]\n");
     exit(0);
   }
   FILE * in = fopen(argv[1],"r");
+  FILE * GMD = 0;
   if(!in){
     perror("Could not open input file");
     exit(1);
+  }
+  if(argc >= 4){
+    GMD = fopen(argv[3],"r");
+    if(!GMD){
+      perror("Could not GMD file");
+    }
   }
   int width,height;
   fread((void *)&width,sizeof(int),1,in);
@@ -43,9 +91,22 @@ int main(int argc, char ** argv){
 	}
       }
       free(data);
-      sprintf(buffer,"%s-%d.tif",argv[2],bunch_id);
-      //      sp_image_write(image,buffer,COLOR_JET);
+      //      append_tag(buffer,argv[2],bunch_id);
+      GMDData * gd = get_values_from_GMD(GMD,bunch_id);
+      char buffer2[1024];
+      char buffer3[1024];
+      sprintf(buffer2,"%d",bunch_id);
+      append_tag(buffer3,argv[2],buffer2);   
+      if(!gd){
+	append_tag(buffer,buffer3,"xxxx");
+      }else{
+	sprintf(buffer2,"%f",gd->e_hall);
+	append_tag(buffer,buffer3,buffer2);
+	free(gd);
+      }
+      sp_image_write(image,buffer,COLOR_JET);
       sp_image_free(image);
+      exit(0);
     }
   }else{
     /* We have the single image case */ 
@@ -60,10 +121,24 @@ int main(int argc, char ** argv){
 	i++;		   
       }
     }
-    sp_image_write(image,argv[2],COLOR_JET);
     ExtraData ed;
     fread(&ed,sizeof(ExtraData),1,in);
     printf("Timestamp - %d\n",ed.timestamp);
+    GMDData * gd = get_values_from_GMD(GMD,ed.timestamp);
+    char buffer[1024];
+    char buffer2[1024];
+    char buffer3[1024];
+    sprintf(buffer2,"%d",ed.timestamp);
+    append_tag(buffer3,argv[2],buffer2);   
+    if(!gd){
+      append_tag(buffer,buffer3,"xxxx");
+    }else{
+      sprintf(buffer2,"%f",gd->e_hall);
+      append_tag(buffer,buffer3,buffer2);
+      free(gd);
+    }
+    sp_image_write(image,buffer,COLOR_JET);
+    
   }
   return 0;
 }
