779d23d44e2aea208c25100764ea4e7be79efc10
ekeberg <ekeberg@3f9d24b3-d911-0410-9091-ffa98efebf22>
2009-03-16 12:37:27 +0000

    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@466 3f9d24b3-d911-0410-9091-ffa98efebf22

--- src/utils/analyse_mask.c
+++ src/utils/analyse_mask.c
@@ -37,10 +37,7 @@ Options * parse_options(int argc, char ** argv){
       strcpy(res->amplitudes,optarg);
       break;
     case 'o':
-      printf("o\n");
       strcpy(res->output,optarg);
-      printf("done o\n");
-      printf("output = %s\n",res->output);
       break;
     case 'h':
       printf("%s",help_text);
@@ -50,7 +47,6 @@ Options * parse_options(int argc, char ** argv){
       printf ("?? getopt returned character code 0%o ??\n", c);
     }
   }
-  printf("end while\n");
   if(!res->pattern[0]){
     fprintf(stderr,"You HAVE to specify a pattern!\n");    
     flag = 1;
@@ -62,7 +58,6 @@ Options * parse_options(int argc, char ** argv){
   if(flag){
     exit(1);
   }
-  printf("return res\n");
   return res;
 }
 
@@ -73,7 +68,6 @@ int main(int argc, char **argv)
   Options * opts = malloc(sizeof(Options));
   //set_defaults(opts);
   opts = parse_options(argc,argv);
-  printf("options done %s\n",opts->output);
 
   Image *pattern = sp_image_read(opts->pattern,0);
   Image *amplitudes = sp_image_read(opts->amplitudes,0);
@@ -93,7 +87,6 @@ int main(int argc, char **argv)
   Image *inside_mask = sp_image_alloc(sp_image_x(pattern),sp_image_y(pattern),1);
   Image *outside_mask = sp_image_alloc(sp_image_x(pattern),sp_image_y(pattern),1);
   for (i = 0; i < max_i; i++) {
-    //printf("mask = (%g,%g)\n",sp_real(mask->image->data[i]),sp_imag(mask->image->data[i]));
     if (amplitudes->mask->data[i] > 0.0) {
       outside_mask->image->data[i] = pattern->image->data[i];
       inside_mask->image->data[i] = sp_cinit(0.0,0.0);
@@ -134,51 +127,3 @@ int main(int argc, char **argv)
   sp_image_free(inside_fft);
   sp_image_free(outside_fft);
 }
-
-/*
-int main(int argc, char **argv)
-{
-  int i;
-
-  Image *img = sp_image_read(argv[1],0);
-  const int max_x = sp_image_x(img);
-  const int max_y = sp_image_y(img);
-  const int max_i = sp_image_size(img);
-  Image *mask_full_size_s = sp_image_read(argv[2],0);
-  mask_full_size_s->detector->image_center[0] = (real) atoi(argv[4]);
-  mask_full_size_s->detector->image_center[1] = (real) atoi(argv[5]);
-  Image *mask_full_size = sp_image_shift(mask_full_size_s);
-  real downsamp = (real) sp_image_x(mask_full_size) / (real) (sp_image_x(img) + 2*atoi(argv[3]));
-  printf("downsamp = %d / %d = %g\n",sp_image_x(mask_full_size), sp_image_x(img) + 2*atoi(argv[3]), downsamp);
-  Image *mask_full_samp = sp_image_low_pass(mask_full_size,(real)atoi(argv[3]) * downsamp,SP_2D);
-  Image *mask = bilinear_rescale(mask_full_samp,max_x,max_y,1);
-  sp_image_free(mask_full_size);
-  
-  Image *inside_mask = sp_image_alloc(max_x,max_y,1);
-  Image *outside_mask = sp_image_alloc(max_x,max_y,1);
-  for (i = 0; i < max_i; i++) {
-    //printf("mask = (%g,%g)\n",sp_real(mask->image->data[i]),sp_imag(mask->image->data[i]));
-    if (sp_real(mask->image->data[i]) > 0.0) {
-      outside_mask->image->data[i] = img->image->data[i];
-      inside_mask->image->data[i] = sp_cinit(0.0,0.0);
-    } else {
-      outside_mask->image->data[i] = sp_cinit(0.0,0.0);
-      inside_mask->image->data[i] = img->image->data[i];
-    }
-  }
-
-  Image *inside_fft = sp_image_fft(inside_mask);
-  Image *outside_fft = sp_image_fft(outside_mask);
-
-  sp_image_write(sp_image_shift(inside_mask),"inside_mask.png",COLOR_JET);
-  sp_image_write(sp_image_shift(outside_mask),"outside_mask.png",COLOR_JET);
-  sp_image_write(inside_fft,"inside_fft.png",COLOR_JET|LOG_SCALE);
-  sp_image_write(inside_fft,"inside_fft_phase.png",COLOR_WHEEL|COLOR_WEIGHTED_PHASE|LOG_SCALE);
-  sp_image_write(outside_fft,"outside_fft.png",COLOR_JET|LOG_SCALE);
-  sp_image_write(outside_fft,"outside_fft_phase.png",COLOR_WHEEL|COLOR_WEIGHTED_PHASE|LOG_SCALE);
-  sp_image_free(inside_mask);
-  sp_image_free(outside_mask);
-  sp_image_free(inside_fft);
-  sp_image_free(outside_fft);
-}
-*/
