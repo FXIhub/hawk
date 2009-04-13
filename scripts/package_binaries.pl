e893f68e1a06e58a2d8efeaa7a1d019b486ba074
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2009-04-13 19:43:45 +0000

    Changed standard Qt linking and improved package_binaries
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@510 3f9d24b3-d911-0410-9091-ffa98efebf22

--- scripts/package_binaries.pl
+++ scripts/package_binaries.pl
@@ -1,10 +1,76 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -d
 
 # This script assumes no intervention is necessary on cmake
+
 
 use strict;
 use Cwd 'abs_path';
 use File::Basename;
+
+sub get_dependencies{
+  my $bin = shift;
+  my $use_ldd = 0;
+  if(`which ldd`){
+    $use_ldd = 1;
+  }
+  my $libs;
+  if($use_ldd ){
+    $libs = `ldd $bin`;
+  }else{
+    $libs = `otool -L $bin`;
+  }
+  my @lines = split("\n",$libs);
+  my @deps;
+  foreach my $line (@lines){
+    my $lib;
+    if($use_ldd){
+      $line =~ /.*? => (.*?) \(0x[0-9abcdef]+\)/;
+      $lib = $1;
+    }else{
+      $line =~ /\s*(.*dylib)/;
+      $lib = $1;
+    }
+    if($lib ne ""){
+      push(@deps, $lib);
+    }
+  }
+  return @deps;
+}
+
+sub get_dependencies_recursively{
+  my %deps;
+  my $bin = shift;
+  my @out = get_dependencies($bin);
+  while(scalar(@out)){
+    my $dep = pop(@out);
+    unless($deps{$dep}){
+      $deps{$dep} = 1;
+      push(@out,get_dependencies($dep));
+    }
+  }
+  return keys(%deps);
+}
+
+sub get_all_dependencies{
+  my $bindir = shift;
+  my $dir = `pwd`;
+  chdir($bindir);
+  my $f = `ls -1`;
+  my @files = split("\n",$f);
+  my %deps;
+#  print @files;
+  foreach my $file(@files){
+    my @deps =  get_dependencies_recursively($file);
+    foreach my $dep(@deps){
+      $deps{$dep} = 1;
+    }
+  }
+  foreach my $key(keys %deps){
+    print $key."\n";
+  }
+  chdir($dir);
+  return keys %deps;
+}
 
 my $basedir =  dirname(abs_path(__FILE__));
 (my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) =
@@ -18,6 +84,9 @@ my $builddir = ($basedir."/../releases/hawk-build-$arch");
 `mkdir -p $reldir`;
 `mkdir -p $builddir`;
 $reldir = abs_path($reldir);
+my $libdir = $reldir."/lib/";
+my $scriptdir = $reldir."/scripts/";
+`mkdir -p $scriptdir`;
 $builddir = abs_path($builddir);
 print $builddir;
 chdir($builddir) or die($!);
@@ -28,3 +97,9 @@ system("make install");
 chdir($reldir);
 mkdir("lib");
 chdir("lib");
+my @deps = get_all_dependencies("../bin");
+foreach my $dep(@deps){
+  system("cp $dep $libdir");
+}
+system("cp $basedir/hawkrc* $scriptdir");
+system("cp $basedir/setup.pl $reldir");
