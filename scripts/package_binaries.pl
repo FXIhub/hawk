427b7c9ed3f80dfd580d93357f4239abb6d91323
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2009-04-13 14:49:10 +0000

    First version package
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@506 3f9d24b3-d911-0410-9091-ffa98efebf22

--- scripts/package_binaries.pl
+++ scripts/package_binaries.pl
@@ -1,6 +1,10 @@
 #!/usr/bin/perl -w
 
+# This script assumes no intervention is necessary on cmake
+
 use strict;
+use Cwd 'abs_path';
+use File::Basename;
 
 my $basedir =  dirname(abs_path(__FILE__));
 (my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) =
@@ -8,6 +12,17 @@ my $basedir =  dirname(abs_path(__FILE__));
 
 my $version = sprintf("%02d.%02d.%02d",$year%100,$mon,$mday);
 my $arch = `uname -sm|tr " /" "_"`;
-my $reldir = $basedir."../releases/hawk-$version-$arch";
-`mkdir -p $reldir`
-
+chomp($arch);
+my $reldir = abs_path($basedir."/../releases/hawk-$version-$arch");
+my $builddir = abs_path($basedir."/../releases/hawk-build-$arch");
+`mkdir -p $reldir`;
+`mkdir -p $builddir`;
+print $builddir;
+chdir($builddir) or die($!);
+print `pwd`;
+system("cmake ../.. -DCMAKE_INSTALL_PREFIX:PATH=$reldir");
+system("make -j 4");
+system("make install");
+chdir($reldir);
+mkdir("lib");
+chdir("lib");
