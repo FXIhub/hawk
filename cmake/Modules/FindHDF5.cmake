126727854703a5609852077d59c0fc54dcf62370
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2009-04-23 08:43:28 +0000

    Tried to improve cmake module
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@543 3f9d24b3-d911-0410-9091-ffa98efebf22

--- cmake/Modules/FindHDF5.cmake
+++ cmake/Modules/FindHDF5.cmake
@@ -7,10 +7,19 @@
 # also defined, but not for general use are
 #  HDF5_LIBRARY, where to find the HDF5 library.
 
-FIND_PATH(HDF5_INCLUDE_DIR hdf5.h)
+IF(WIN32)
+SET(INCLUDE_PATHS c:/MinGW/)
+SET(LIB_PATHS c:/MinGW/)
+ELSEIF(APPLE)
+SET(INCLUDE_PATHS /sw/)
+SET(LIB_PATHS /sw/)
+ELSE()
+SET(INCLUDE_PATHS /usr/local/)
+SET(LIB_PATHS /usr/local/)
+ENDIF()
 
-SET(HDF5_NAMES ${HDF5_NAMES} hdf5)
-FIND_LIBRARY(HDF5_LIBRARY NAMES ${HDF5_NAMES} )
+FIND_PATH(HDF5_INCLUDE_DIR hdf5.h PATHS ${INCLUDE_PATHS} PATH_SUFFIXES include)
+FIND_LIBRARY(HDF5_LIBRARY NAMES ${HDF5_NAMES} PATHS ${LIB_PATHS} PATH_SUFFIXES lib)
 
 # handle the QUIETLY and REQUIRED arguments and set HDF5_FOUND to TRUE if 
 # all listed variables are TRUE
