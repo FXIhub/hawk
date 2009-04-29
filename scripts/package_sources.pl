ce288ec9a67a07a7e56a6108eded28bc007de7d7
filipe <filipe@3f9d24b3-d911-0410-9091-ffa98efebf22>
2009-04-29 16:53:58 +0000

    Added a package_sources script
    git-svn-id: svn://hirst.bmc.uu.se/hawk/trunk@559 3f9d24b3-d911-0410-9091-ffa98efebf22

---
+++ scripts/package_sources.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+# This script assumes no intervention is necessary on cmake
+
+
+use strict;
+use File::Basename;
+use File::Spec;    
+
+my $basedir =  dirname( File::Spec->rel2abs(__FILE__));
+(my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,my $isdst) =
+                                                               localtime(time);
+my $version = sprintf("%02d.%02d.%02d",$year%100,$mon+1,$mday);
+my $arch = `gcc -dumpmachine`;
+chomp($arch);
+my $reldir =($basedir."/../releases/hawk-$version-src");
+`rm -rf $reldir`;
+$reldir = File::Spec->rel2abs($reldir);
+chdir($basedir."/../releases");
+system("svn export svn://hirst/hawk/trunk $reldir");
+system("tar -zcvf ".$reldir.".tar.gz hawk-$version-src");
