15b46bae1d2f6bf5e16163e59ee8086966ac8550
Filipe Maia <filipe@xray.bmc.uu.se>
2009-09-30 15:42:54 +0200

    Always output stuff in linear scale

--- src/utils/image_to_png.c
+++ src/utils/image_to_png.c
@@ -14,16 +14,16 @@ int main(int argc, char ** argv){
   if(a->shifted){
     a = sp_image_shift(a);
   }
-    sp_image_write(a,argv[2],SpColormapJet|SpColormapLogScale);
+    sp_image_write(a,argv[2],SpColormapJet);
     //  sp_image_write(a,argv[2],SpColormapJet);
   if(argc > 3){
-    sp_image_write(a,argv[3],SpColormapJet|SpColormapLogScale|SpColormapPhase);
+    sp_image_write(a,argv[3],SpColormapJet|SpColormapPhase);
   }
   if(argc > 4){
     for(i = 0;i<sp_c3matrix_size(a->image);i++){
       a->image->data[i] = sp_cinit(a->mask->data[i],0);
     }
-    sp_image_write(a,argv[4],SpColormapJet|SpColormapLogScale);
+    sp_image_write(a,argv[4],SpColormapJet);
   }
   return 0;
 }
