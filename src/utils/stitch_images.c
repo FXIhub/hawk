75c0f98f1185c76dff5694d26f2fd9b53d5cab4e
Filipe Maia <filipe@xray.bmc.uu.se>
2009-10-21 17:26:56 +0200

    Make user specify output file

--- src/utils/stitch_images.c
+++ src/utils/stitch_images.c
@@ -63,22 +63,22 @@ void find_corners(ImageGeometry * geom, Image ** images, int n_images, int * x0,
   sp_vector_free(Tp);
 
   for(int i = 0;i<n_images;i++){
-    for(int x = 0; x<=sp_image_x(images[i]);x+=sp_image_x(images[i])){
-      for(int y = 0; y<=sp_image_y(images[i]);y+=sp_image_y(images[i])){
+    for(int x = 0; x<sp_image_x(images[i]);x+=sp_image_x(images[i])-1){
+      for(int y = 0; y<sp_image_y(images[i]);y+=sp_image_y(images[i])-1){
 	sp_vector_set(p,0,x);
 	sp_vector_set(p,1,y);
 	Tp = apply_affine_transform(geom[i].t,p);
-	if(sp_vector_get(Tp,0) < *x0){
-	  *x0 = sp_vector_get(Tp,0);
+	if(floor(sp_vector_get(Tp,0)) < *x0){
+	  *x0 = floor(sp_vector_get(Tp,0));
 	}
-	if(sp_vector_get(Tp,1) < *y0){
-	  *y0 = sp_vector_get(Tp,1);
+	if(floor(sp_vector_get(Tp,1)) < *y0){
+	  *y0 = floor(sp_vector_get(Tp,1));
 	}
-	if(sp_vector_get(Tp,0) > *x1){
-	  *x1 = sp_vector_get(Tp,0);
+	if(ceil(sp_vector_get(Tp,0)) > *x1){
+	  *x1 = ceil(sp_vector_get(Tp,0));
 	}
-	if(sp_vector_get(Tp,1) > *y1){
-	  *y1 = sp_vector_get(Tp,1);
+	if(ceil(sp_vector_get(Tp,1)) > *y1){
+	  *y1 = ceil(sp_vector_get(Tp,1));
 	}
 	printf("%g x %g\n",sp_vector_get(Tp,0),sp_vector_get(Tp,1));
 	sp_vector_free(Tp);
@@ -106,42 +106,49 @@ Image * stitch_images(ImageGeometry * geom, Image ** images, int n_images){
       sp_vector_set(pos,0,x);
       sp_vector_set(pos,1,y);
       real v = 0;
+      int mask = 0;
+      sp_image_mask_set(ret,x-x0,y-y0,0,0);
       for(int i = 0;i<n_images;i++){
 	sp_vector * t_pos = apply_affine_transform(geom[i].t_inv,pos);
+	//	sp_vector * t_pos = apply_affine_transform_inverse(geom[i].t,pos);
 	real rx = sp_vector_get(t_pos,0);
 	real ry = sp_vector_get(t_pos,1);
 	if(sp_image_contains_coordinates(images[i],rx,ry,0)){
 	  v = sp_image_interp(images[i],rx,ry,0);
+	  mask = 1;
 	  break;
 	  //	  printf("%f x %f inside image %d\n",rx,ry,i);
 	}
       }
       sp_image_set(ret,x-x0,y-y0,0,sp_cinit(v,0));
+      sp_image_mask_set(ret,x-x0,y-y0,0,mask);
     }
   }
+  ret->detector->image_center[0] = -(x0-1);
+  ret->detector->image_center[1] = -(y0-1);
   return ret;
 }
 
 int main(int argc, char ** argv){
-  if(argc < 4){
-    printf("Usage: %s <geometry file> <imageA> <imageB> ...\n",argv[0]);
+  if(argc < 5){
+    printf("Usage: %s <geometry file> <output image> <imageA> <imageB> ...\n",argv[0]);
     exit(1);
   }
   int n_images;
   ImageGeometry * geom = read_geometry_file(argv[1],&n_images);
 
-  if(n_images != argc-2){
+  if(n_images != argc-3){
     fprintf(stderr,"Number of input images doesn't match geometry specification!\n");
     abort();
   }
   check_geometry(geom,n_images);
 
   Image ** images = malloc(sizeof(Image*)*n_images);
-  for(int i = 2;i<argc;i++){
-    images[i-2] = sp_image_read(argv[i],0);
+  for(int i = 3;i<argc;i++){
+    images[i-3] = sp_image_read(argv[i],0);
   }
 
   Image * stitched = stitch_images(geom,images,n_images);
-  sp_image_write(stitched,"stitched.h5",0);
+  sp_image_write(stitched,argv[2],0);
   return 0;
 }
