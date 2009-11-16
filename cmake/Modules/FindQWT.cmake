a7344b453e76470c20117676454801c43aa2dd16
Filipe Maia <filipe@xray.bmc.uu.se>
2009-11-16 18:37:45 +0100

    Fix qxt compile problem, and fix problems introduced by qwt5.2 packaged in a different way.

--- cmake/Modules/FindQWT.cmake
+++ cmake/Modules/FindQWT.cmake
@@ -1,7 +1,7 @@
 # - Find QWT library
 # Find the native QWT includes and library
 # This module defines
-#  QWT_INCLUDE_DIR, where to find hdf5.h, etc.
+#  QWT_INCLUDE_DIR, where to find qwt.h, etc.
 #  QWT_LIBRARIES, libraries to link against to use QWT.
 #  QWT_FOUND, If false, do not try to use QWT.
 # also defined, but not for general use are
@@ -12,11 +12,14 @@ SET(EXTRA_PREFIX c:/MinGW/)
 ELSEIF(APPLE)
 SET(EXTRA_PREFIX /sw/)
 ELSE()
+# For some reason they decided to change the location of the include files in 5.2
+SET(EXTRA_PREFIX /usr/include/qwt-qt4)
 ENDIF()
 
 FIND_PATH(QWT_INCLUDE_DIR qwt.h PATHS ${EXTRA_PREFIX} PATH_SUFFIXES include)
 
-SET(QWT_NAMES ${QWT_NAMES} qwt5 qwt)
+# They also decided to name it qwt-qt4, which also breaks some stuff
+SET(QWT_NAMES ${QWT_NAMES} qwt-qt4 qwt5 qwt)
 FIND_LIBRARY(QWT_LIBRARY NAMES ${QWT_NAMES} PATHS ${EXTRA_PREFIX} PATH_SUFFIXES lib)
 
 # handle the QUIETLY and REQUIRED arguments and set QWT_FOUND to TRUE if 
